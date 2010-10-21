f8a731e21745134c4d487b395e1241d304b267d3
Surakarn Samkaew <tonkla@gmail.com>
2010-10-21 12:27:17 +0700

    Add JavaScript localization for en_US

---
+++ scripts/locale/en.js
@@ -0,0 +1,12 @@
+Locale = function() {
+    this.And = "AND";
+    this.Or = "OR";
+    this.Not = "NOT";
+    this.Title = "Title";
+    this.Subject = "Subject";
+    this.Author = "Author";
+    this.ISBN = "ISBN";
+    this.CallNumber = "Call Number";
+    this.AddAnotherField = "Add another field";
+}
+
