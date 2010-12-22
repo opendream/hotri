f13d09ca666e8382a6df235c61232e3a97ad4418
Surakarn Samkaew <tonkla@gmail.com>
2010-12-22 11:06:11 +0700

    Add credits text

--- home/credits.php
+++ home/credits.php
@@ -15,11 +15,5 @@
   }
 ?>
 
-<br />
-<em>OpenBiblio core 0.6.0 <?php echo $headerLoc->getText("footerDatabaseVersion"); ?> <?php echo H(OBIB_DB_VERSION);?></em>
-<br />
-<?php echo $headerLoc->getText("footerCopyright"); ?> &copy; 2002-2005 Dave Stevens
-<br />
-
 <?php
 include("../shared/footer.php");
