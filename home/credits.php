064f39cec3bbf71ad16fcae089c68529965407eb
Surakarn Samkaew <tonkla@gmail.com>
2010-12-22 11:24:39 +0700

    Fix credits wordings

--- home/credits.php
+++ home/credits.php
@@ -3,10 +3,13 @@
   $tab = "home";
   $nav = "credits";
 
+  require_once("../classes/Localize.php");
+  $navloc = new Localize(OBIB_LOCALE, "navbars");
+
   include("../shared/header.php");
 ?>
 
-<h1>Credits</h1>
+<h1><?php echo $navloc->getText("homeCreditsLink"); ?></h1>
 
 <?php
   $lines = file("../credits.txt");
